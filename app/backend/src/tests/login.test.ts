fcd9c6d9042fbf20316a214694693347afaf15ff
hector <hectoreddbenitez@gmail.com>
2022-06-06 16:38:31 -0300

    UPDATE: adding endpoint login teste

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -15,51 +15,61 @@ import { Response } from 'superagent';
 const { expect } = chai;
 
 describe('Testando endpoint /login', () => {
-  let chaiHttpResponse: Response;
+  describe('Quando é feita corretamente a requisição, com email e password válidos', async () => {
+    let chaiHttpResponse: Response;
 
-  before(async () => {
-    sinon.stub(UserModel, 'findOne').resolves({
-      id: 1,
-        username: "Admin",
-        role: "admin",
-        email: "admin@admin.com",
-        password:"$2a$12$tENzvmLDTPQ0XN3JxKBfwuck6sgEb7D9Czg7fGNMG5ssdN.JMJZky",
-    } as UserModel);
+    before(async () => {
+      sinon.stub(UserModel, 'findOne').resolves({
+        id: 1,
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+        password:
+          '$2a$12$tENzvmLDTPQ0XN3JxKBfwuck6sgEb7D9Czg7fGNMG5ssdN.JMJZky',
+      } as UserModel);
+    });
+
+    after(() => {
+      (UserModel.findOne as sinon.SinonStub).restore();
+    });
+
+    it('a resposta é um objeto com status code 200, e as chaves user e token', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: 'admin@admin.com', password: '123456' });
+
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.have.property('user');
+      expect(chaiHttpResponse.body).to.have.property('token');
+      expect(chaiHttpResponse.body.user).to.not.have.property('password');
+    });
   });
 
-  after(() => {
-    (UserModel.findOne as sinon.SinonStub).restore();
-  });
+  describe('Quando é feita a requisição, com um email inválido', async () => {
+    let chaiHttpResponse: Response;
 
-  it('Quando é feita corretamente a requisição, com email e password válidos', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({ email: 'admin@admin.com', password: '123456' });
-    
-    expect(chaiHttpResponse).to.have.status(200);
-    expect(chaiHttpResponse.body).to.have.property('user');
-    expect(chaiHttpResponse.body).to.have.property('token');
-    expect(chaiHttpResponse.body.user).to.not.have.property('password');
-  });
+    before(async () => {
+      sinon.stub(UserModel, 'findOne').resolves();
+    });
 
-  // it('Quando é feita a requisição, com um email inválido', async () => {
-  //   before(async () => {
-  //     sinon.stub(UserModel, 'findOne').resolves({ } as UserModel);
-  //   });
-  
-  //   after(() => {
-  //     (UserModel.findOne as sinon.SinonStub).restore();
-  //   });
-  //   chaiHttpResponse = await chai
-  //     .request(app)
-  //     .post('/login')
-  //     .send({ email: 'errado@errado.com', password: '123456' });
-      
-  //   expect(chaiHttpResponse).to.have.status(401);
-  //   expect(chaiHttpResponse.body).to.not.have.property('user');
-  //   expect(chaiHttpResponse.body).to.not.have.property('token');
-  //   expect(chaiHttpResponse.body).to.have.property('message');
-  //   expect(chaiHttpResponse.body.message).to.be('Incorrect email or password');
-  // });
+    after(() => {
+      (UserModel.findOne as sinon.SinonStub).restore();
+    });
+
+    it('a resposta é um objeto com status code 401 e a chave message', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: 'errado@errado.com', password: '123456' });
+
+      expect(chaiHttpResponse).to.have.status(401);
+      expect(chaiHttpResponse.body).to.not.have.property('user');
+      expect(chaiHttpResponse.body).to.not.have.property('token');
+      expect(chaiHttpResponse.body).to.have.property('message');
+      expect(chaiHttpResponse.body.message).to.be.equal(
+        'Incorrect email or password'
+      );
+    });
+  });
 });
